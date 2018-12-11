a1afe48ce3d83f4b3151b878283e0c2ff9c6a058
Adarsh Chaudhary <adarshrohit217@gmail.com>
2018-12-12 04:38:08 +0530

    Add files via upload

---
+++ js/verb.js
@@ -0,0 +1,13 @@
+new TypeIt('#verb', {
+    strings:    ["<span class='develop'>MAKE</span>", 
+    "<span class='code'>CODE</span>", 
+    "<span class='invent'>DEBUG</span>", 
+    "<span class='question'>QUESTION</span>", 
+    "<span class='make'>ANSWER</span>"],
+    speed: 120,
+    breakLines: false,
+    loop: true,
+    autoStart: true,
+    nextStringDelay: 500,
+    deleteSpeed: 50,
+});
